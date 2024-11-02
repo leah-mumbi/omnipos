40d391524bdc668a79b4e4a77d72988db34584f8
Leah <mumbileah254@gmail.com>
2024-11-02 20:47:50 +0300

    add bootsrap

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>omnipos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
     <style>
     *{
         margin:0;
@@ -47,25 +49,36 @@
             <li>
                 <h1>Orders</h1>
                 <ul>
-                    <li><h2>New order</h2><li>
-                    <li>All orders<li>
+                    <li><h3>New order</h3><li>
+                    <li><h3>All orders<h3><li>
                 </ul>
             </li>
             <li>
                 <h1>Inventory</h1>
                 <ul>
                     <li>
-                        <h2>Categories</h2>
+                        <h3>Categories</h3>
                         <ul>
                             <li>New Category<li>
                             <li>All categories<li>
                             <li>Delete Category<li>
                         </ul>
-                    <li>
-                    <li>All orders<li>
+                          
+                    
+                    <li> 
+                        <h3>Products</h3>
+                        <ul>
+                            <li>New Product</li>
+                            <li>All products</li>
+                            <li>Delete Product</li>
+                        </ul>
+<li>
                 </ul>
             </li>
-            <li>Reports</li>
+            <li>
+                <h1>Reports</h1>
+                    
+            </li>
             <li>User Management</li>
             <li>Profile</li>
             <li>Log out</li>
