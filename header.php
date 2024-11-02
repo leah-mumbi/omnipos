236994705dfcfed4d1fb6bfe0ab8685492fa027a
Leah <mumbileah254@gmail.com>
2024-11-02 20:28:24 +0300

    fixed layout bug (missing style closing tag and DOCTYPE declarion

--- header.php
+++ header.php
@@ -1,102 +1,73 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<style>
-*{
-    margin:0;
-    padding:0;
-}
-body{
-  height: 100vh;
-  display: grid;
-  grid-template-areas:
-    'menu header header header header header header header'
-    'menu main main main main main main main'
-    'menu main main main main main main main'
-    'menu main main main main main main main'
-    'menu main main main main main main main';
-  background-color: #ffffff;
-}
-
-<<<<<<< HEAD
-header{
-	grid-area: header;
-    height: 10vh;
-	background-color: gray;
-}
-
-aside{
-	grid-area: menu;
-	background-color:green;
-}
-=======
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>omnipos</title>
     <style>
-        h1 {
-            margin-bottom: 20px;
-              text-transform: uppercase;
-                word-spacing: 10px;
-                text-align: center;
-                width: 100%;
-                
-        }
+    *{
+        margin:0;
+        padding:0;
+    }
+    body{
+    height: 100vh;
+    display: grid;
+    grid-template-areas:
+        'menu header header header header header header header'
+        'menu main main main main main main main'
+        'menu main main main main main main main'
+        'menu main main main main main main main'
+        'menu main main main main main main main';
+    background-color: #ffffff;
+    }
 
-        .container-fluid {
-            margin-top: 30px; 
-        
-        }
->>>>>>> 540acc7 (changes)
+    header{
+        grid-area: header;
+        height: 10vh;
+        background-color: gray;
+    }
 
-main{
-	grid-area: main;
-	height: 90vh;
-    background-color: brown;
-}
+    aside{
+        grid-area: menu;
+        background-color:green;
+    }
 
-<<<<<<< HEAD
-</style>
-=======
-        .navbar-nav .nav-link {
-            margin-right: 15px;
-            /* Add space between links */
-        }
-
-        .navbar-nav .nav-link:last-child {
-            margin-right: 0;
-            /* Remove margin from the last link */
-        }
-        body{
-              text-align: center;
-        }
+    main{
+        grid-area: main;
+        height: 90vh;
+        background-color: brown;
+    }
     </style>
->>>>>>> 540acc7 (changes)
-</head>
-<body class="grid-container">
-  <header>Header</header>
-  <aside>
-      <ul>
-          <li>
-              <h1>Orders</h1>
-              <ul>
-                  <li><h2>New order</h2><li>
-                  <li>All orders<li>
-              </ul>
-          </li>
-          <li>
-              <h1>Inventory</h1>
-              <ul>
-                  <li>
-                      <h2>Categories</h2>
-                      <ul>
-                          <li>New Category<li>
-                          <li>All categories<li>
-                          <li>Delete Category<li>
-                      </ul>
-                  <li>
-                  <li>All orders<li>
-              </ul>
-          </li>
-          <li>Reports</li>
-          <li>User Management</li>
-          <li>Profile</li>
-          <li>Log out</li>
-      </ul>
-  </aside>
+
+    </head>
+    <body class="grid-container">
+    <header>Header</header>
+    <aside>
+        <ul>
+            <li>
+                <h1>Orders</h1>
+                <ul>
+                    <li><h2>New order</h2><li>
+                    <li>All orders<li>
+                </ul>
+            </li>
+            <li>
+                <h1>Inventory</h1>
+                <ul>
+                    <li>
+                        <h2>Categories</h2>
+                        <ul>
+                            <li>New Category<li>
+                            <li>All categories<li>
+                            <li>Delete Category<li>
+                        </ul>
+                    <li>
+                    <li>All orders<li>
+                </ul>
+            </li>
+            <li>Reports</li>
+            <li>User Management</li>
+            <li>Profile</li>
+            <li>Log out</li>
+        </ul>
+    </aside>
